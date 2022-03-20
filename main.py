6903b6a7c1893124bd851b76ce37fb5e9e62deb0
ivan <ivanbelousov14@gmail.com>
2022-03-20 20:45:20 +0300

    second commit > homework is done (ver1)

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from utils import load_candidates_from_json
+from utils import load_candidates_from_json, get_candidate, get_candidates_by_name, get_candidates_by_skills
+from flask import Flask, render_template
 from settings import path
-from flask import Flask
-from utils import get_candidate
-from utils import get_candidates
 
 
 app = Flask(__name__)
 @app.route("/")
 def page_index():
+    candidates = load_candidates_from_json(path)
+    return render_template("list.html", candidates=candidates)
+
+
+@app.route("/candidate/<int:uid>")
+def profile(uid):
+    candidate = get_candidate(uid)
+    return render_template("card.html", candidate=candidate)
+
+
+@app.route("/search/<candidate_name>")
+def search(candidate_name):
+    candidates = get_candidates_by_name(candidate_name)
+    return render_template("search.html", candidates=candidates)
+
+
+@app.route("/skills/<skills_name>")
+def skill(skills_name):
+    candidates = get_candidates_by_skills(skills_name)
+    return render_template("skills.html", candidates=candidates, skill=skills_name)
 
 
 app.run()
--- utils.py
+++ utils.py
@@ -8,48 +8,36 @@ def load_candidates_from_json(path):
         data = json.load(file)
     return data
 
-
-def get_candidates():
-    """Возвращает всех кандидатов в читабельном формате"""
-    candidates = load_candidates_from_json(path)
-    all_candidates = ""
-    for candidate in candidates:
-        all_candidates += f"<strong>Имя:</strong> {candidate['name']}<br><img src={candidate['picture']}><br><strong>Вакансия:</strong> {candidate['position']}<br><strong>Пол:</strong> {candidate['gender']}<br><strong>Возраст:</strong> {candidate['age']}<br><strong>Навыки:</strong> {candidate['skills']}<br><hr><br>"
-    return all_candidates
-
-
+__data = load_candidates_from_json(path)
 def get_candidate(candidate_id):
     """Возвращает кандидата по ID"""
-    data = load_candidates_from_json(path)
-    str_candidate = ""
-    for d in data:
+    for d in __data:
         if d["id"] == candidate_id:
-            str_candidate += f"Имя: {d['name']}<br><img src={d['picture']}><br>Вакансия: {d['position']}<br>Пол:{d['gender']}<br>Возраст:{d['age']}<br>Навыки: {d['skills']}"
-    return str_candidate
+            return {
+                "name": d["name"],
+                "picture": d["picture"],
+                "position": d["position"],
+                "skills": d["skills"]
+            }
+    return {"error": "Кандидат не найден"}
 
 
 def get_candidates_by_name(candidate_name):
     """Возвращает кандидата(ов) по имени"""
-    candidates = load_candidates_from_json(path)
-    str_candidate = ""
-    for candidate in candidates:
-        name = candidate["name"].split()
-        if candidate_name.title() == name[0]:
-            str_candidate += f"Имя: {candidate['name']}<br><img src={candidate['picture']}><br>Вакансия: {candidate['position']}<br>Пол:{candidate['gender']}<br>Возраст:{candidate['age']}<br>Навыки: {candidate['skills']}<br>"
-    return str_candidate
+    return [candidate for candidate in __data if candidate_name.lower() in candidate["name"].lower()]
+
 
 
 def get_candidates_by_skills(skill_name):
     """Возвращает кандидата(ов) по скилу"""
-    candidates = load_candidates_from_json(path)
-    str_candidate = ""
-    for candidate in candidates:
-        skills_str = candidate["skills"]
-        skills_list = skills_str.split()
-        for skill in skills_list:
-            if skill_name.lower() in skill.lower():
-                str_candidate += f"{candidate['name']}<br>{candidate['skills']}<br>"
-    return str_candidate
+
+    candidates = []
+    for candidate in __data:
+        candidate["skills"]
+        skills_list = candidate["skills"].lower().split(", ")
+        if skill_name in skills_list:
+            candidates.append(candidate)
+    return candidates
 
 
 
